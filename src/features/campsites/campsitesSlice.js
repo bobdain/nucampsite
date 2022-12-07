d60e0db7e7204a36618d575149a0d00c7c15730f
Bob Dain <bob.dain@se.com>
2022-12-06 23:10:28 -0800

    using state to select campsite details

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,12 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+export const selectCampsitesById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 }
+
+
+
+// export const selectRandomCampsite = () => {
+//     return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+// }
