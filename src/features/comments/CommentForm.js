16d0974c0f5ba96accde4ad35159a596740d76a0
Bob Dain <bob.dain@se.com>
2022-12-16 21:46:16 -0800

    part 3

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, FormGroup, Label } from 'reactstrap';
-import { Formik, Field, Form } from 'formik';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import validateCommentForm from '../../utils/validateCommentForm';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
@@ -34,7 +35,8 @@ const CommentForm = ({ campsiteId }) => {
                             rating: undefined,
                             author: '',
                             commentText: ''
-                        }} onSubmit={handleSubmit} >
+                        }} onSubmit={handleSubmit}
+                            validate={validateCommentForm}>
                             <Form>
 
                                 {/* Rating */}
@@ -54,6 +56,9 @@ const CommentForm = ({ campsiteId }) => {
                                         <option>4</option>
                                         <option>5</option>
                                     </Field>
+                                    <ErrorMessage name='rating' >
+                                        {(msg) => <p className='text-danger'>{msg}</p>}
+                                    </ErrorMessage>
                                 </FormGroup>
 
                                 {/* Author */}
@@ -66,6 +71,9 @@ const CommentForm = ({ campsiteId }) => {
                                         placeholder='Your Name'
                                         className='form-control'
                                     />
+                                    <ErrorMessage name='author'>
+                                        {(msg) => <p className='text-danger'>{msg}</p>}
+                                    </ErrorMessage>
                                 </FormGroup>
 
                                 {/* Comment */}
